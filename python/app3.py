7c6a1f1cffe8944b54615ff0294d9c025bd8ca40
sharmimathi123 <144650008+sharmimathi123@users.noreply.github.com>
2023-09-12 13:51:28 +0530

    Add files via upload

---
+++ python/app3.py
@@ -0,0 +1,10 @@
+name="sarmila"
+address="sukkalampatty kombai"
+tamil=90
+english=89
+maths=98
+science=99
+social=97
+total=(tamil+english+maths+science+social)
+average=total/5
+print("my name is:",name,"/n","my address is:",address,"/n","tamil mark:",tamil,"/n","english mark :",english,"/n","maths mark :",maths,"/n","science mark:",science,"/n","social mark:",social,"\n","my average is:",average,"/n","my total marks :",total,"\n")
